f699a4eba92c776ea813d414c6ebcfbfa14e31f0
GiovanniLafortezza <recordeknot388@gmail.com>
2024-01-03 16:54:43 +0100

    initial commit

--- hello-spring-mvc/src/main/java/org/lessons/java/hellospringmvc/controller/MainController.java
+++ hello-spring-mvc/src/main/java/org/lessons/java/hellospringmvc/controller/MainController.java
@@ -1,6 +1,7 @@
 package org.lessons.java.hellospringmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class MainController {
     @GetMapping
-    public String home() {
+    public String home(Model model) {
         return "home";
     }
 }
