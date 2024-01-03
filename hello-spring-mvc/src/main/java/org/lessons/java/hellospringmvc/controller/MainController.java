82ad47de9d33f89578ce2203acc113b8e446bce4
GiovanniLafortezza <recordeknot388@gmail.com>
2024-01-03 17:09:20 +0100

    add method random and print number

--- hello-spring-mvc/src/main/java/org/lessons/java/hellospringmvc/controller/MainController.java
+++ hello-spring-mvc/src/main/java/org/lessons/java/hellospringmvc/controller/MainController.java
@@ -5,11 +5,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Random;
+
 @Controller
 @RequestMapping("/")
 public class MainController {
     @GetMapping
     public String home(Model model) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(1,7);
+        model.addAttribute("randomNumber", randomNumber);
         return "home";
     }
 }
